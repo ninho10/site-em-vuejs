9487cdf1ba98a3edff360e98c38ec8f898c9ee6b
Paulo Roberto <paulo.trabalho10@gmail.com>
2023-04-07 15:59:17 -0300

    site em vue
    estou criando eventos..

--- meu-projeto/vue-project/src/App.vue
+++ meu-projeto/vue-project/src/App.vue
@@ -2,16 +2,19 @@
   <div>
   <primeiro-componente/>
   <p>O dade de ep e {{ testando }}</p>
+  <life-cycle/>
  </div>
 </template>
 
 <script>
 import primeiroComponente from './components/primeiroComponente.vue';
+import lifeCycle from './components/lifeCycle.vue'; 
 
 export default{
   name: 'App',
   components: {
-    primeiroComponente
+    primeiroComponente,
+    lifeCycle
   },
   data(){
     return {
---
+++ meu-projeto/vue-project/src/components/lifeCycle.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h1>Meu nome e : {{ nome }}</h1>
+
+    </div>
+</template>
+
+
+<script>
+    export default {
+        name: 'lifeCycle',
+        data() {
+            return {
+                nome: 'Ainda nao sei'
+            }
+        },
+        created() {
+            setTimeout(() => {
+                this.nome = 'Paulo Roberto' 
+            },2000);
+           
+        },
+        mounted() {
+            setTimeout(() => {
+                this.nome = 'Ninho' 
+            },4000);
+        }
+    }
+
+</script>
